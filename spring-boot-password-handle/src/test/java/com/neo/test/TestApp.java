2e03f4a11c76a592bf3b5518dd6a74dfc398b789
784277265@qq.com <784277265@qq.com>
2019-11-24 18:56:54 +0800

    修改信息工具类

--- spring-boot-password-handle/src/test/java/com/neo/test/TestApp.java
+++ spring-boot-password-handle/src/test/java/com/neo/test/TestApp.java
@@ -1,10 +1,7 @@
 package com.neo.test;
 
-import com.alibaba.fastjson.util.TypeUtils;
 import com.google.common.base.Stopwatch;
 import com.neo.PasswordHandleApplication;
-import com.neo.modifyinfo.Property;
-import com.neo.modifyinfo.ReflectUtils;
 import com.neo.service.PasswordService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,11 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Proxy;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 
@@ -42,33 +34,5 @@ public class TestApp {
         
         System.out.println("字典破解："+plaintext+"，耗时："+stopwatch.elapsed());
     }
-    
-    @Test
-    public void test03() throws NoSuchFieldException , IllegalAccessException {
-        TestBean bean = TestBean.builder()
-                .string("String")
-                .integer(1)
-                .date(TypeUtils.castToDate("2019-11-22 21:29:34"))
-                .bigDecimal(TypeUtils.castToBigDecimal("10"))
-                .aBoolean(true)
-                .build();
 
-        Field field = ReflectUtils.getFieldByFieldName(bean.getClass(), "date");
-
-        Property property = field.getAnnotation(Property.class);
-
-        Class<? extends Annotation> nameAnnotationClass = (Class<? extends Annotation>) property.nameAnnotationClass();
-
-        Annotation nameAnnotation = field.getAnnotation(nameAnnotationClass);
-
-        InvocationHandler invocationHandler = Proxy.getInvocationHandler(nameAnnotation);
-        
-        Field memberValuesField = invocationHandler.getClass().getDeclaredField("memberValues");
-        memberValuesField.setAccessible(true);
-
-        Map map = (Map) memberValuesField.get(invocationHandler);
-        
-        System.out.println("FieldName :" + map.get(property.nameAnnotationClassField()));
-
-    }
 }
